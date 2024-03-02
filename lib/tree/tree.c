f49cbc9ab73e4b6d25e1f7fe5f54b51075f4f8c4
Agon <agon-kolgeci@hotmail.com>
2024-03-02 23:14:27 +0100

    TP3

--- lib/tree/tree.c
+++ lib/tree/tree.c
@@ -5,4 +5,172 @@
 #include "./../queue/queue.h"
 #include "./../stack/stack.h"
 
-// TODO: Implement
+Tree *tree_create() {
+    Tree *tree = malloc(sizeof(Tree));
+    if(tree == NULL) {
+        return NULL;
+    }
+
+    tree->root = NULL;
+
+    return tree;
+}
+
+TreeNode *tree_search(Tree *tree, void *data, int (*cmp)(void *, void *)) {
+    TreeNode *current = tree->root;
+
+    while(current != NULL) {
+        int cmp_result = cmp(current->data, data);
+
+        switch (cmp_result) {
+            case 0: {
+                return current;
+            }
+
+            case -1: {
+                current = current->left;
+
+                break;
+            }
+
+            case 1: {
+                current = current->right;
+
+                break;
+            }
+        }
+
+    }
+
+    return NULL;
+}
+
+void tree_destroy(Tree *tree) {
+    Stack *stack = stack_create();
+    TreeNode *current = tree->root;
+
+    while(current != NULL) {
+        TreeNode *tmp = current;
+
+        if(current->left != NULL) {
+            stack_push(stack, tmp);
+            
+            current = current->left;
+            tmp->left = NULL;
+        } else if(current->right != NULL) {
+            stack_push(stack, tmp);
+
+            current = current->right;
+            tmp->right = NULL;
+        } else {
+            free(tmp);
+
+            current = stack_pop(stack);
+        }
+    }
+
+    tree->root = NULL;
+
+    stack_destroy(stack);
+    free(tree);
+}
+
+void tree_insert(Tree *tree, void *data, int (*cmp)(void *, void *)) {
+    TreeNode *newNode = malloc(sizeof(TreeNode));
+    if(newNode == NULL) {
+        return;
+    }
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    if(tree->root == NULL) {
+        tree->root = newNode;
+
+        return;
+    }
+
+    TreeNode *current = tree->root;
+    TreeNode *parent = NULL;
+
+    while(current != NULL) {
+        switch (cmp(current->data, data)) {
+            case 0: {
+                free(newNode);
+
+                return;
+            }
+            
+            case -1: {
+                parent = current;
+                current = current->left;
+
+                break;
+            }
+
+            case 1: {
+                parent = current;
+                current = current->right;
+
+                break;
+            }
+        }
+    }
+
+    if(cmp(parent->data, data) < 0) {
+        parent->left = newNode;
+    } else {
+        parent->right = newNode;
+    }
+}
+
+List *tree_bfs(Tree *tree) {
+    List *list = list_create();
+    Queue *queue = queue_create();
+
+    queue_enqueue(queue, tree->root);
+
+    while(queue_size(queue) != 0) {
+        TreeNode *current = queue_dequeue(queue);
+
+        list_append(list, current->data);
+
+        if(current->right != NULL) {
+            queue_enqueue(queue, current->right);
+        }
+
+        if(current->left != NULL) {
+            queue_enqueue(queue, current->left);
+        }
+    }
+
+    queue_destroy(queue);
+
+    return list;
+}
+
+List *tree_dfs(Tree *tree) {
+    List *list = list_create();
+    Stack *stack = stack_create();
+
+    stack_push(stack, tree->root);
+
+    while(stack_size(stack) != 0) {
+        TreeNode *current = stack_pop(stack);
+
+        list_append(list, current->data);
+
+        if(current->left != NULL) {
+            stack_push(stack, current->left);
+        }
+
+        if(current->right != NULL) {
+            stack_push(stack, current->right);
+        }
+    }
+
+    stack_destroy(stack);
+
+    return list;
+}
--- src/tp3/main.c
+++ src/tp3/main.c
@@ -3,8 +3,46 @@
 #include "./../../lib/list/list.h"
 #include "./../../lib/tree/tree.h"
 
-int main()
-{
+/*
+
+   This file is for debugging only.
+
+*/
+
+int int_cmp(void *a, void *b) {
+    int x = (int) a;
+    int y = (int) b;
+    if(x < y){
+        return -1;
+    } else if(x > y){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    Tree *tree = tree_create();
+
+    tree_insert(tree, (void *) 10, int_cmp);
+    tree_insert(tree, (void *) 5, int_cmp);
+    tree_insert(tree, (void *) 20, int_cmp);
+    tree_insert(tree, (void *) 4, int_cmp);
+    tree_insert(tree, (void *) 6, int_cmp);
+    tree_insert(tree, (void *) 19, int_cmp);
+    tree_insert(tree, (void *) 21, int_cmp);
+
+    List *list = tree_bfs(tree);
+    for(int i = 0; i < list_size(list); i++){
+        printf("%d | ", ((int *) list_get(list, i)));
+    }
+
+    List *list = tree_dfs(tree);
+    for(int i = 0; i < list_size(list); i++){
+        printf("%d | ", ((int *) list_get(list, i)));
+    }
+
+    tree_destroy(tree);
 
     return 0;
 }
